e13efac34b7114926aef71d2a36dd3370f5cae08
Sergio <sjarmero@gmail.com>
2021-05-03 20:14:16 +0200

    Support for multiline translations

--- PoHTTPLoader.ts
+++ PoHTTPLoader.ts
@@ -15,12 +15,16 @@ export class PoHTTPLoader implements TranslateLoader {
         );
     }
 
-    private parse(contents: string): any {
+    public parse(contents: string): any {
         const translations: { [key: string]: string } = {};
 
         let key = '';
         let translation = '';
-        for (const line of contents.split('\n')) {
+
+        const lines = contents.split('\n');
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+
             // Ignore comments and blank lines
             if (line.indexOf('#') === 0 || line === '') {
                 continue;
@@ -34,15 +38,44 @@ export class PoHTTPLoader implements TranslateLoader {
                     key = line.substring(begin + 1, end);
                     translation = '';
                 } else {
+                    // Multi line ID
+                    let stop = false;
+                    let j = i + 1;
                     key = '';
-                }
-            }
+                    while (!stop) {
+                        if (lines[j] !== '' && lines[j].indexOf('msgstr') === -1 && lines[j].indexOf('#') !== 0) {
+                            key += lines[j].substr(1, lines[j].length - 2);
+                            j++;
+                        } else {
+                            stop = true;
+                        }
+                    }
 
-            if (line.indexOf('msgstr') !== -1) {
+                    i = j - 1;
+                }
+            } else if (line.indexOf('msgstr') !== -1) {
                 const begin = line.indexOf('"');
                 const end = line.lastIndexOf('"');
 
-                translation = end - begin > 1 ? line.substring(begin + 1, end) : '';
+                if (end - begin > 1) {
+                    translation = line.substring(begin + 1, end);
+                } else {
+                    // Multi line ID
+                    let stop = false;
+                    let j = i + 1;
+                    translation = '';
+                    while (!stop) {
+                        if (lines[j] !== '' && lines[j].indexOf('msgid') === -1 && lines[j].indexOf('#') === -1) {
+                            translation += lines[j].substr(1, lines[j].length - 2);
+                        } else {
+                            stop = true;
+                        }
+
+                        j++;
+                    }
+
+                    i = j - 1;
+                }
             }
 
             if (key.length > 0 && translation.length > 0) {
